92824166d3d76ea41b4c0ac98aba6a4fe17b2654
martinps206 <martinps_cc@hotmail.com>
2023-09-30 09:55:36 -0300

    Migrando la version de React, con respecto a nuvas librerias

---
+++ springboot-backend/src/main/java/com/martinps/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.martinps.repository;
+
+import com.martinps.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
